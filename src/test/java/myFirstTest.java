81497e3c75f8c13b3d02edd0b181481e36dc1c41
Александр Придача <alex322p9@gmail.com>
2018-07-25 23:53:04 +0300

    Java. Класс, открывающий все ссылки меню
    сценарий, проходящий по всем разделам админки

---
+++ src/test/java/myFirstTest.java
@@ -0,0 +1,64 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class myFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://localhost/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        int menuSize = driver.findElements(By.xpath("//*[@id=\"box-apps-menu\"]/li/a")).size();
+
+        for (int i = 0; i < menuSize; i++) {
+            List<WebElement> menuItems = driver.findElements(By.xpath("//*[@id=\"box-apps-menu\"]/li/a"));
+            menuItems.get(i).click();
+
+            int subMenuSize = driver.findElements(By.cssSelector("li#app- ul a")).size();
+
+            for (int j = 0; j < subMenuSize; j++) {
+
+                List<WebElement> subMenuItems = driver.findElements(By.cssSelector("li#app- ul a"));
+                subMenuItems.get(j).click();
+            }
+        }
+
+
+
+
+
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+
+}
